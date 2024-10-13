575dc30a7a96ea78ec64bf15944040c96b31bb37
tusharverma06 <vermatushar237@gmail.com>
2024-10-13 20:22:17 +0530

    feat: 🎸 setting all states back to default on exit

--- src/components/BarcodeScanner.tsx
+++ src/components/BarcodeScanner.tsx
@@ -139,6 +139,7 @@ export default function BoardingPassScanner() {
         });
         setIsLoading(false);
         setIsScannerOpen(false);
+        setScannedData("")
       } else if (lifeCycleRes.statusName === "error") {
         toast({
           title: "Error",
